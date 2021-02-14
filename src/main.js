3365057bf078873c694b1a6adeea45abf23dcf29
Marcin Górski <marcin.gorski@mobilereality.pl>
2021-02-14 21:50:51 +0100

    [feat] install vuex|axios|vue-meta

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueMeta from "vue-meta";
+import Vuex from "vuex";
+import store from "@/store";
+
+Vue.use(Vuex);
+
+Vue.use(VueMeta, {
+  refreshOnceOnNavigation: true
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
