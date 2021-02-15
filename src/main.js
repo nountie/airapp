04643562e21b01abdb07c058a6b59997b5f16afb
Marcin Górski <marcin.gorski@mobilereality.pl>
2021-02-15 10:49:22 +0100

    [feat] install v-click-outside

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueMeta from "vue-meta";
+import vClickOutside from "v-click-outside";
 import Vuex from "vuex";
 import store from "@/store";
 
+Vue.use(vClickOutside);
 Vue.use(Vuex);
 
 Vue.use(VueMeta, {
